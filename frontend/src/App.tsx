323c057beaa8dcc8dc92d78ca3c5c4a162e99e10
Aiden Castillo <aidencastillo41@gmail.com>
2025-09-03 13:37:23 -0600

    Made the button slightly more responsive to the screen size

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,6 @@ function App() {
     const [imagePath, setImagePath] = useState<string>("");
     const [asciiText, setAsciiText] = useState<string[]>([]);
 
-    // TODO - Make the UI look better
     async function convertToAscii() {
         let bytes = await FetchImageAsBytes();
         if (!bytes) return; // Prevents error from empty string
@@ -25,7 +24,8 @@ function App() {
         <main id="App" className="flex flex-col gap-4 justify-start bg-black h-screen w-screen">
             <h1 className="pt-4 text-2xl font-bold underline">Image to ASCII Converter</h1>
             <div>
-                <button className="bg-gradient-to-b from-blue-500 to-purple-500 rounded-md size-36 cursor-pointer
+                <button className="bg-gradient-to-b from-blue-500 to-purple-500 rounded-md cursor-pointer px-2
+                                    w-[25vw] min-w-[5rem] max-w-[10rem] h-[25vh]  min-h-[5rem]  max-h-[10rem]
                                     hover:bg-gradient-to-l hover:from-red-500 hover:to-yellow-500" 
                                     id="image-file" onClick={convertToAscii}>
                     <p className="font-bold">Upload Your Photo</p>
